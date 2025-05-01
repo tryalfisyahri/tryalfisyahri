ba38883e9fedc76942f85e50e6f7ca03ed81d73c
tryalfisyahri <tryalfisyahri01@gmail.com>
2025-05-01 08:54:08 +0700

    Create tabel perkalian.php

---
+++ tabel perkalian.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gambar Tabel Perkalian</title>
+    <style>
+        body {
+            text-align: center;
+            font-family: Arial, sans-serif;
+        }
+        img {
+            margin-top: 20px;
+            max-width: 90%;
+            height: auto;
+            border: 1px solid #aaa;
+        }
+    </style>
+</head>
+<body>
+
+<h2>Tabel Perkalian 1 sampai 10 (Gambar)</h2>
+<img src="tabel_perkalian.png" alt="Tabel Perkalian 1 sampai 10">
+
+</body>
+</html>
